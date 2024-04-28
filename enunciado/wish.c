e639db7b89e8cfb348c31089056608064c856821
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-04-28 12:08:26 -0500

    Test 7 passed

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -90,71 +90,88 @@ Vector parse_input(char *expresion){
     return items;
 }
 
-// Commands functions
-void my_exit(Vector items){
-    // Test 5: Tries to exit with an argument. Should throw an error. (Exit con más de un argumento)
-    if(items.size > 1){
-        print_error();
-    } else {
-        exit(0);
+// Handle commands functions
+int handle_builtin_commands(Vector items){
+    if (strcmp(vector_get(&items, 0), "exit") == 0){
+        if(items.size > 1){
+            print_error();            
+        } else {
+            exit(0);
+        }
+        return 1;
+    } else if (strcmp(vector_get(&items, 0), "cd") == 0) {
+        if(chdir(vector_get(&items, 1)) != 0){
+            // Test 1 and 2: Input to check bad cd. No arguments or 2 arguments are passed to cd.
+            print_error();
+        }
+        return 1;
+    } else if (strcmp(vector_get(&items, 0), "path") == 0){
+        Vector new_path = vector_create();
+
+        for (int i = 1; i < items.size; i++){
+            char* aux = (char*) malloc((strlen(vector_get(&items, i)) + strlen("/") + 1) * sizeof(char));
+            strcpy(aux, vector_get(&items, i));
+            strcat(aux, "/");
+
+            // Agregar el nuevo string a local_path
+            vector_add(&new_path, aux);
+        }
+
+        PATH = new_path;
+        return 1;
     }
+    return 0;
 }
 
-void cd(Vector items){
-    if(chdir(vector_get(&items, 1)) != 0){
-        // Test 1 and 2: Input to check bad cd. No arguments or 2 arguments are passed to cd.
-        print_error();
-    }
-}
-
-void ls(Vector items){
+int handle_external_commands(Vector items){
     char *command = vector_get(&items, 0);
-
-    if (fork() == 0) {
-        // Proceso hijo
-        // Ejecutar el comando externo
-        execvp(command, items.data);
+    int pos = vector_get_index(&items, ">");
+    if (pos == -1) pos = items.size;
+    for (int i = 0; i < PATH.size; i++) {
+        char *p = (char*) malloc((strlen(vector_get(&PATH, i)) + strlen(command) + 1) * sizeof(char));
+        snprintf(p, strlen(vector_get(&PATH, i)) + strlen(command) + 1, "%s%s", vector_get(&PATH, i), command);
         
-        print_error();
-    } else {
-        // Proceso padre
-        // Esperar a que el hijo termine
-        wait(NULL);
-    }
-}
-
-void path(Vector items){
-    Vector new_path = vector_create();
-    for (int i = 1; i < items.size; i++){
-        vector_add(&new_path, vector_get(&items, i));
-    } 
-    PATH = new_path;
-}
-
-void handle_scripts(Vector items){
-    char *script = vector_get(&items, 0);
-    for(int i = 0; i < PATH.size; i++) {
-        char *dir = malloc(strlen(vector_get(&PATH, i)) + strlen(script) + 2); // Tamaño suficiente para almacenar la ruta completa
-        if (dir == NULL) {
-            perror("Error en malloc");
-            exit(EXIT_FAILURE);
+        if (access(p, X_OK) == 0) {
+            char* argv[pos+1];
+            for (int i = 0; i < pos; i++) argv[i] = vector_get(&items, i);
+            argv[pos] = NULL;
+            
+            int rc = fork();
+            if (rc == 0) {
+                if (pos != items.size) {
+                    close(STDOUT_FILENO);
+                    open(
+                        vector_get(&items, items.size - 1),
+                        O_CREAT | O_WRONLY | O_TRUNC,
+                        S_IRWXU
+                    );
+                }
+                execv(p, argv);
+            }
+            return rc;
         }
-        snprintf(dir, strlen(vector_get(&PATH, i)) + strlen(script) + 2, "%s/%s", vector_get(&PATH, i), script);
-
-        // Verificar si el archivo existe
-        if (access(dir, X_OK) == 0) {
-            printf("El archivo '%s' existe en '%s'\n", script, vector_get(&PATH, i));
-        }
-        
-        free(dir); // Liberar memoria asignada
     }
 
     print_error();
+    return -1;   
+}
+
+char *search_script(char *filename){
+    for (int i = 0; i < PATH.size; i++){
+        char *dir = (char*) malloc((strlen(vector_get(&PATH, i)) + strlen(filename) + 1) * sizeof(char));
+        snprintf(dir, strlen(vector_get(&PATH, i)) + strlen(filename) + 1, "%s%s", vector_get(&PATH, i), filename);
+        if(access(dir, X_OK) == 0){
+            return dir;
+        }
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[]){
     char expression[256];
     Vector items;
+    char *filename;
+    int in_exec;
 
     vector_add(&PATH, "./");
     vector_add(&PATH, "/usr/bin/");
@@ -187,19 +204,13 @@ int main(int argc, char *argv[]){
 
         items = parse_input(expression);
 
-        if(strcmp(vector_get(&items, 0), "exit") == 0){
-            my_exit(items);
-        } else if (strcmp(vector_get(&items, 0), "cd") == 0) {
-            cd(items);
-        } else if (strcmp(vector_get(&items, 0), "ls") == 0) {
-            ls(items);
-        } else if (strcmp(vector_get(&items, 0), "path") == 0){
-            
-        } else {
-            handle_scripts(items);
-        }
+        in_exec = handle_builtin_commands(items);
 
-        free(items.data);
+        if(in_exec == 0){
+            handle_external_commands(items);
+        }        
+
+        vector_destroy(&items);
     }
 }
 
